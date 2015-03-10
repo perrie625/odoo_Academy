37b7efad998498327edbcf2ab6b981d8758b03e2
Perry <q_perry@163.com>
2015-03-10 10:06:42 +0800

    URLs and routing '/academy/<model("academy.teachers"):teacher>/'

--- controllers.py
+++ controllers.py
@@ -9,9 +9,11 @@ class Academy(http.Controller):
             'teachers': Teachers.search([])
         })
 
-    @http.route('/academy/<int:id>/', auth='public', website=True)
-    def teacher(self, id):
-        return '<h1>{} ({})</h1>'.format(id, type(id).__name__)
+    @http.route('/academy/<model("academy.teachers"):teacher>/', auth='public', website=True)
+    def teacher(self, teacher):
+        return http.request.render('academy.biography', {
+            'person': teacher
+        })
 
 
 # class Academy(http.Controller):
