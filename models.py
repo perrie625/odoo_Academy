f661972b345ab19f7fc0d5b7276281c9005209b7
Perry <perry@b2cstock.com>
2015-03-10 11:34:27 +0800

    Relations between models(courses & teachers)

--- models.py
+++ models.py
@@ -6,6 +6,13 @@ class Teachers(models.Model):
     _name = 'academy.teachers'
     name = fields.Char()
     biography = fields.Html()
+    course_ids = fields.One2many('academy.courses', 'teacher_id', string="Courses")
+
+class Courses(models.Model):
+    _name = 'academy.courses'
+
+    name = fields.Char()
+    teacher_id = fields.Many2one('academy.teachers', string="Teacher")
 
 # class academy(models.Model):
 #     _name = 'academy.academy'
