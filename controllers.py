d63f0ce470810ee6728a72d7b9a3bce8a0eb517b
Perry <q_perry@163.com>
2015-03-10 09:56:53 +0800

    URLs and routing academy/<name>

--- controllers.py
+++ controllers.py
@@ -8,6 +8,11 @@ class Academy(http.Controller):
         return http.request.render('academy.index', {
             'teachers': Teachers.search([])
         })
+
+    @http.route('/academy/<name>/', auth='public', website=True)
+    def teacher(self, name):
+        return '<h1>{}</h1>'.format(name)
+
 # class Academy(http.Controller):
 #     @http.route('/academy/academy/', auth='public')
 #     def index(self, **kw):
