36bf1543810e69f874cc37db8539dcef1d5dc81d
Perry <q_perry@163.com>
2015-03-10 10:14:56 +0800

    Field edition with a placeholder

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from openerp import models, fields, api
 class Teachers(models.Model):
     _name = 'academy.teachers'
     name = fields.Char()
+    biography = fields.Html()
 
 # class academy(models.Model):
 #     _name = 'academy.academy'
