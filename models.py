39145d48bd213e000c8744469c509397a98e0f8b
Perry <perry@b2cstock.com>
2015-03-10 14:12:52 +0800

    Selling courses (error)

--- models.py
+++ models.py
@@ -6,13 +6,10 @@ class Teachers(models.Model):
     _name = 'academy.teachers'
     name = fields.Char()
     biography = fields.Html()
-    course_ids = fields.One2many('academy.courses', 'teacher_id', string="Courses")
+    course_ids = fields.One2many('product.template', 'teacher_id', string="Courses")
 
 class Courses(models.Model):
-    _name = 'academy.courses'
-    _inherit = 'mail.thread'
-
-    name = fields.Char()
+    _inherit = 'product.template'
     teacher_id = fields.Many2one('academy.teachers', string="Teacher")
 
 # class academy(models.Model):
