ca896bce3e7d39f5dccbeef7213f0c6d11b724e4
Perry <q_perry@163.com>
2015-03-10 09:54:01 +0800

    website support

--- controllers.py
+++ controllers.py
@@ -2,7 +2,7 @@
 from openerp import http
 
 class Academy(http.Controller):
-    @http.route('/academy/academy/', auth='public')
+    @http.route('/academy/academy/', auth='public', website=True)
     def index(self, **kw):
         Teachers = http.request.env['academy.teachers']
         return http.request.render('academy.index', {
