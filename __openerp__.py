3f14ef3bcee562ee8448bda1cdd1f32dc3e27070
Perry <perry@b2cstock.com>
2015-03-10 13:09:02 +0800

    use website_sale

--- __openerp__.py
+++ __openerp__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['website'],
+    'depends': ['website_sale'],
 
     # always loaded
     'data': [
