c0bf171b1d57bb399e8bf64abb4539d65d5acf2a
Perry <q_perry@163.com>
2015-03-10 09:40:49 +0800

    "Templates" show teachers list

--- controllers.py
+++ controllers.py
@@ -4,7 +4,9 @@ from openerp import http
 class Academy(http.Controller):
     @http.route('/academy/academy/', auth='public')
     def index(self, **kw):
-        return "hello perry"
+        return http.request.render('academy.index', {
+            'teachers': ["Diana Padilla", "Jody Caroll", "Lester Vaughn"],
+        })
 # class Academy(http.Controller):
 #     @http.route('/academy/academy/', auth='public')
 #     def index(self, **kw):
