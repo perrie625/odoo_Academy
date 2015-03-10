87688372db77dd1eb6c0494b33bf30ab04039559
Perry <q_perry@163.com>
2015-03-10 09:36:04 +0800

    "To the browser" say hello perry

--- controllers.py
+++ controllers.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from openerp import http
 
+class Academy(http.Controller):
+    @http.route('/academy/academy/', auth='public')
+    def index(self, **kw):
+        return "hello perry"
 # class Academy(http.Controller):
 #     @http.route('/academy/academy/', auth='public')
 #     def index(self, **kw):
